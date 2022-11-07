ef5b72e673c7fc760edf4d76ce11c5554b7c47f8
alex231 <alex231@ukr.net>
2022-11-07 17:50:23 +0200

    Add PostgreS to awersome_blog

--- myawersomeblog/settings.py
+++ myawersomeblog/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.apps.EventsConfig',
-    'django.contrib.staticfiles',
+    'events.apps.EventsConfig',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +76,12 @@ WSGI_APPLICATION = 'myawersomeblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myawersomeblogdb',
+        'USER': 'postgres',
+        'PASSWORD': '407702584a',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
