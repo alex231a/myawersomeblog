9ec4db61aef516828cc75f025f9a5f937bb405f2
oyakovenko <alex231@ukr.net>
2023-08-11 16:29:52 +0300

    Add postgres

--- myawersomeblog/settings.py
+++ myawersomeblog/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'myawersomeblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myawersomeblogdb',
+        'USER': 'postgres',
+        'PASSWORD': '407702584a',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
