e4acb3f99f076ec73b5d07883656f5d4cb3ec29a
oyakovenko <alex231@ukr.net>
2023-08-14 16:27:37 +0300

    Final version with requirements.txt

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,6 @@ class Post(models.Model):
 
     def get_summary(self):
         return self.blog_text[:70]
+
+    def __str__(self):
+        return self.blog_title
--- events/models.py
+++ events/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Event(models.Model):
     event_image = models.ImageField(upload_to='event_images/')
     event_text = models.CharField(max_length=300)
+
+    def get_summary(self):
+        return self.event_text[:70]
+
+    def __str__(self):
+        return self.get_summary()
